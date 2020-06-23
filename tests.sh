09acc1c9d54e3e45f3515c1afa53a117635cb68c
Tom <thomas.verin@sonarsource.com>
2020-06-23 14:28:43 +0200

    refactor test

--- tests.sh
+++ tests.sh
@@ -7,15 +7,16 @@ for dir in rules/*
 do
     dir=${dir%*/} 
     echo ${dir##*/}
-    if test -f "$dir/metadata.json"; then
+    FILE="$dir/metadata.json"
+    if test -f $FILE; then
       echo "$FILE exists."
-      HAS_TITLE=`cat $dir/metadata.json | jq 'has("title")'`
+      HAS_TITLE=`cat $FILE | jq 'has("title")'`
       if [ "$HAS_TITLE" != "true" ]; then
-        echo "$dir/metadata.json has no title"
+        echo "$FILE has no title"
         exit_code=1
       fi
     else
-      echo "no $dir/metadata.json"
+      echo "no $FILE"
       exit_code=1
     fi
 done
