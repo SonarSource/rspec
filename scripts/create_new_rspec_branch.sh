4f7f56a13d09fb7e1fc0258dd8af7a08eae7d598
Nicolas Harraudeau <40498978+nicolas-harraudeau-sonarsource@users.noreply.github.com>
2020-09-24 15:42:14 +0200

    Improve 'Create New RSPEC' Github Action

---
+++ scripts/create_new_rspec_branch.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Create the new Rule in a branch
+# It is used by the Github Action script "create_new_rspec.yml".
+
+# Stop script in case of error.
+set -e
+
+RSPEC_ID=$1
+LANGUAGES=$2
+RULES_DIRECTORY=$3
+
+scripts_dir=$(dirname "$0")
+template_dir="${scripts_dir}/rspec_template"
+rule_directory="${RULES_DIRECTORY}/S${RSPEC_ID}"
+
+mkdir $rule_directory
+cp $template_dir/common/* $rule_directory/
+
+for language in $(echo $LANGUAGES | sed "s/,/ /g")
+do
+    mkdir $rule_directory/$language
+    cp $template_dir/language_specific/* $rule_directory/$language/
+done
+
+cd $rule_directory
+
+grep -rl '${RSPEC_ID}' . | xargs sed -i '' "s/\${RSPEC_ID}/${RSPEC_ID}/g"
