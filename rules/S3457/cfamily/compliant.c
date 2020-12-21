ed53c1610bfea182d447ae36f0db44db1f84ae05
Arseniy Zaostrovnykh <arseniy.zaostrovnykh@sonarsource.com>
2020-12-21 15:38:52 +0100

    Add all rules, update all rules fixing the inline code syntax

--- rules/S3457/cfamily/compliant.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    // tag::include[]
-    printf("%d %d", 1, 2); // Compliant
-    printf("%-f", 1.2); // Compliant
-    // end::include[]
-} 
--- rules/S3457/cfamily/noncompliant.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    // tag::include[]
-    printf("%d", 1, 2); // Noncompliant; the second argument "2" is unused
-    printf("%0-f", 1.2); // Noncompliant; flag "0" is ignored because of "-"
-    // end::include[]
-}
