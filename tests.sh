5c5e6d418a30048643153719ac7ab0d07649b091
Tom <thomas.verin@sonarsource.com>
2021-02-05 15:25:11 +0100

    Fix checklinks.py dir

--- tests.sh
+++ tests.sh
@@ -47,7 +47,7 @@ do
         cat $RULE >> $TMP_ADOC
         if asciidoctor --failure-level=WARNING $TMP_ADOC; then
           echo "$RULE syntax is fine"
-          pipenv run python checklinks.py
+          pipenv run python ./rspec-tools/rspec-tools/checklinks.py
         else
           echo "ERROR: $RULE has incorrect asciidoc"
           exit_code=1
