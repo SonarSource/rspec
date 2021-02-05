907fdd2f555b4fed5947a9faf651bbc8d7602007
Tom <thomas.verin@sonarsource.com>
2021-02-05 15:09:47 +0100

    Run link validation in QA

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
-FROM debian:buster-slim
+FROM python:3.9-slim-buster
 
 RUN apt-get update && \
     apt-get install -y --no-install-recommends jq php-json-schema asciidoctor
-
+RUN pip install --upgrade pip
+RUN pip install pipenv
+   
 CMD ["bash"]
--- tests.sh
+++ tests.sh
@@ -2,6 +2,10 @@
 set -euo pipefail
 exit_code=0
 
+#setup python
+cd rspec-tools
+pipenv install -e .
+
 for dir in rules/*
 do
   dir=${dir%*/}
@@ -40,8 +44,9 @@ do
         TMP_ADOC="$language/tmp.adoc"
         echo "== Description" > $TMP_ADOC
         cat $RULE >> $TMP_ADOC
-        if asciidoctor --failure-level=WARNING -o /dev/null $TMP_ADOC; then
+        if asciidoctor --failure-level=WARNING $TMP_ADOC; then
           echo "$RULE syntax is fine"
+          pipenv run python checklinks.py
         else
           echo "ERROR: $RULE has incorrect asciidoc"
           exit_code=1
