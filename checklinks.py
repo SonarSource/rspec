1b28503275bbf7d0bef46e34b20bcc504bf90d76
Tom <thomas.verin@sonarsource.com>
2021-02-08 17:17:38 +0100

    Display http code when errored

--- checklinks.py
+++ checklinks.py
@@ -11,26 +11,28 @@ def live_url(url):
   try:
     code = urlopen(url).code  
     if (code / 100 >= 4):
-      print(f"Nothing there: {url}")
-      error=1
+      print(f"Ko: {code} Nothing there: {url}")
+      return False
     else:
       print(f"Ok: {url}")
+      return True
   except URLError:
-    print(f"Could not access: {url}")
-    error=1
+    print(f"Ko: {code} Could not access: {url}")
+    return False
 
-def findurl_in_html(filename):
+def findurl_in_html(filename, pr: bool=False):
   with io.open(filename, 'r', encoding="utf8") as file:
     soup = BeautifulSoup(file,features="html.parser")
     for link in soup.findAll('a'):
-      live_url(link.get('href'))
+      if not live_url(link.get('href')) and pr:
+        exit(1)
 
 
 for directory, dirnames, filenames in os.walk("."):
   for filename in filenames:
     if filename.endswith('.html'):
       findurl_in_html(os.path.join(directory,filename))
-if error == 1:
-  exit(1)
+
+  
 
 
