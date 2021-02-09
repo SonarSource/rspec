c1577a3e99b068494c65e329d4b2a55a6b526fc9
Tom <thomas.verin@sonarsource.com>
2021-02-09 15:46:06 +0100

    Add descritpion before validation

--- validate_asciidoc.sh
+++ validate_asciidoc.sh
@@ -3,7 +3,11 @@ set -euo pipefail
 
 #validate asciidoc
 find rules -type f -name "*.adoc" | while read line; do
-  if ! asciidoctor --failure-level=WARNING -o /dev/null $line; then    
+  #in order to not fail for rspecs that have third-level headers in the description we always add a description
+  TMP_ADOC="$language/tmp.adoc"
+  echo "== Description" > $TMP_ADOC
+  cat $line >> $TMP_ADOC
+  if ! asciidoctor --failure-level=WARNING -o /dev/null $TMP_ADOC; then    
     echo "ERROR: $line has incorrect asciidoc"
     exit_code=1
   fi
