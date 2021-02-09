f3e42af8f90cfc365a41e84093cbf22c3898e3af
Tom <thomas.verin@sonarsource.com>
2021-02-09 15:50:15 +0100

    Fix tmp adoc filename

--- validate_asciidoc.sh
+++ validate_asciidoc.sh
@@ -4,12 +4,13 @@ set -euo pipefail
 #validate asciidoc
 find rules -type f -name "*.adoc" | while read line; do
   #in order to not fail for rspecs that have third-level headers in the description we always add a description
-  TMP_ADOC="$language/tmp.adoc"
+  TMP_ADOC="tmp.adoc"
   echo "== Description" > $TMP_ADOC
   cat $line >> $TMP_ADOC
   if ! asciidoctor --failure-level=WARNING -o /dev/null $TMP_ADOC; then    
     echo "ERROR: $line has incorrect asciidoc"
     exit_code=1
   fi
+  rm $TMP_ADOC
 done
 exit $exit_code
