284cc4640da2e507b2404788e73c72a96097bc4a
Tom <thomas.verin@sonarsource.com>
2021-02-10 09:44:10 +0100

    Fix setup

--- rspec-tools/setup.py
+++ rspec-tools/setup.py
@@ -3,7 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='rspec-tools',
     version='1.0.0',
-    url='https://github.com/SonarSource/rspec',
     author='SonarSource',
     description='Rule specification tooling',
     packages=find_packages(),
