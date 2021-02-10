5004fa0e2b9a470d4e61b0c334c6849a726c218f
Tom <thomas.verin@sonarsource.com>
2021-02-10 09:49:00 +0100

    Pipenv setup before launching script

--- rspec-tools/setup.py
+++ rspec-tools/setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='rspec-tools',
     version='1.0.0',
+    url='https://github.com/SonarSource/rspec',
     author='SonarSource',
     description='Rule specification tooling',
     packages=find_packages(),
--- validate_links.sh
+++ validate_links.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-#setup python
-cd rspec-tools
-pipenv install -e i
-pipenv shell
-cd ..
-
 #validate links in asciidoc
 mkdir -p out
 asciidoctor -R rules -D out '**/*.adoc' 
