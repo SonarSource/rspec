da99caf5d69547b11c7f542a4dc2d6d7a5cbc3c9
Tom <thomas.verin@gmail.com>
2021-02-11 10:09:47 +0100

    Change back global timeout to 2h and set request timeout to 5s

--- rspec-tools/rspec_tools/checklinks.py
+++ rspec-tools/rspec_tools/checklinks.py
@@ -14,10 +14,10 @@ def live_url(filename,url):
     data=None, 
     headers={
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
-    }
+    }    
   )
   try:
-    code = urlopen(req).code  
+    code = urlopen(req,timeout=5).code  
     if (code / 100 >= 4):
       print(f"Ko: {filename} {code} Nothing there: {url}")
       return False
