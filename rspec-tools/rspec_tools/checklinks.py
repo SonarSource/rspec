84fe60ee21e693fe821c77787b438cedc50e137a
Tom <thomas.verin@gmail.com>
2021-02-11 11:46:19 +0100

    Refactor url testing to test only once a link

--- rspec-tools/rspec_tools/checklinks.py
+++ rspec-tools/rspec_tools/checklinks.py
@@ -6,8 +6,11 @@ from urllib.request import urlopen,Request
 from urllib.error import URLError,HTTPError
 import pathlib
 
+def show_files(filenames):
+  for filename in filenames:
+    print(filename)
 
-def live_url(filename,url):
+def live_url(filenames,url):
   code=None
   req = Request(
     url, 
@@ -19,37 +22,53 @@ def live_url(filename,url):
   try:
     code = urlopen(req,timeout=5).code  
     if (code / 100 >= 4):
-      print(f"Ko: {filename} {code} Nothing there: {url}")
+      print(f"ERROR: {code} Nothing there")
+      show_files(filenames)
       return False
     else:
-      print(f"Ok: {url}")
       return True  
   except HTTPError as h:
-    print(f"Ko: {filename} {h.code} {h.reason} {url}")
+    print(f"ERROR: {h.code} {h.reason}")
+    show_files(filenames)
     return False
   except URLError as e:
-    print(f"Ko: {filename} {e.reason} {url}")
+    print(f"ERROR: {e.reason}")
+    show_files(filenames)
     return False
   except ConnectionError as c:
-    print(f"Ko: {filename} {url} connection error {c}")
+    print(f"ERROR: connection error {c}")
+    show_files(filenames)
     return False
 
-def findurl_in_html(filename):    
+def findurl_in_html(filename,urls):    
   ok=True
   with open(filename, 'r', encoding="utf8") as file:
     soup = BeautifulSoup(file,features="html.parser")
     for link in soup.findAll('a'):
-      ok=ok and live_url(filename,link.get('href'))
+      #ok=ok and live_url(filename,link.get('href'))
+      key=link.get('href')
+      if key in urls:
+        urls[key].append(filename)
+      else:
+        urls[key]=[filename]
+
   return ok
   
 
 def check_html_links(dir):  
   error_code=0
+  ok=True
+  urls={}
   for filepath in pathlib.Path(dir).glob('**/*.html'):
     filename=str(filepath.absolute())
-    if not findurl_in_html(filename):
+    if not findurl_in_html(filename,urls):
       error_code=1
   print(f"All html files checked")
+  for key in urls:
+    print(f"{key}:{len(urls[key])}")
+    ok=ok and live_url(urls[key],key)
+  if not ok:
+    print("There were errors")
   exit(error_code)
 
   
