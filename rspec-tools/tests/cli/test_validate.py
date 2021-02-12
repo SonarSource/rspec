4a414a11a2369b1239eeba81e5de979319b9f33c
Tom <thomas.verin@gmail.com>
2021-02-12 11:58:58 +0100

    remove test execution from CI

--- rspec-tools/tests/cli/test_validate.py
+++
@@ -1,8 +0,0 @@
-from click.testing import CliRunner
-from rspec_tools import cli
-
-def test_hello_world():
-  runner = CliRunner()
-  result = runner.invoke(cli, ['--debug', 'validate', '--rule=42'])
-  assert result.exit_code == 1
-  assert result.output == 'Error: No rule has ID 42\n'
