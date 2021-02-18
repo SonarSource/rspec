5744c7b4092ce882d831a6bd3633cc77266cbd83
Nicolas Harraudeau <40498978+nicolas-harraudeau-sonarsource@users.noreply.github.com>
2021-02-18 12:32:10 +0100

    Fix create rule command in create_new_rspec github workflow

--- rspec-tools/rspec_tools/create_rule.py
+++ rspec-tools/rspec_tools/create_rule.py
@@ -7,9 +7,9 @@ from contextlib import contextmanager
 
 from rspec_tools.utils import copy_directory_content
 
-def build_github_repository_url(token):
+def build_github_repository_url(token: str):
   'Builds the rspec repository url'
-  return f'https://{token}:@github.com/SonarSource/rspec.git'
+  return f'https://{token}@github.com/SonarSource/rspec.git'
 
 def extract_repository_name(url):
   url_end = url.split('/')[-2:]
