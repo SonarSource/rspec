7b88dd2468d3d52174b0e46e5d611b4bf605d4e9
Nicolas Harraudeau <40498978+nicolas-harraudeau-sonarsource@users.noreply.github.com>
2021-02-25 11:40:28 +0100

    Validate only rules links

--- validate_links.sh
+++ validate_links.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 #validate links in asciidoc
 mkdir -p out
-asciidoctor -R rules -D out '**/*.adoc' 
+asciidoctor -R rules -D out 'rules/**/*.adoc'
 cd rspec-tools
 pipenv install -e .
 pipenv run rspec-tools check-links --d ../out
