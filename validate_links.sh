07da6f456ec7c9a38fbdf6fb27f60eb8edd2714e
Arseniy Zaostrovnykh <arseniy.zaostrovnykh@sonarsource.com>
2021-04-30 16:02:41 +0200

    Factor out the html generation from the link-checking task

--- validate_links.sh
+++ validate_links.sh
@@ -2,11 +2,6 @@
 set -euo pipefail
 
 #validate links in asciidoc
-mkdir -p out
-asciidoctor -R rules -D out 'rules/*/*/rule.adoc'
-cd rules
-  find . -name 'metadata.json' -exec cp --parents '{}' ../out \;
-cd ..
 cd rspec-tools
 pipenv install -e .
 pipenv run rspec-tools check-links --d ../out
