414872582ac26cde2030df37270f04a76baf2c55
Arseniy Zaostrovnykh <arseniy.zaostrovnykh@sonarsource.com>
2021-05-04 09:54:20 +0200

    Document the ACCEPTED_SECTION_NAMES list

--- rspec-tools/rspec_tools/validation/description.py
+++ rspec-tools/rspec_tools/validation/description.py
@@ -5,6 +5,11 @@ from typing import Final
 from rspec_tools.errors import RuleValidationError
 from rspec_tools.rules import LanguageSpecificRule
 
+# The list of all the sections currently accepted by the script.
+# The list includes multiple variants for each title because they all occur
+# in the migrated RSPECs.
+# Further work required to shorten the list by renaming the sections in some RSPECS
+# to keep only on version for each title.
 ACCEPTED_SECTION_NAMES: Final[list[str]] = ['Noncompliant Code Example',
                                             'Noncompliant Code Example.',
                                             'Noncompliant Code Example:',
