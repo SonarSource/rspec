539ee027ac89fceb26436e3551e2c96c2e782009
Arseniy Zaostrovnykh <arseniy.zaostrovnykh@sonarsource.com>
2021-09-29 11:11:53 +0200

    capitalize constants

--- rule_coverage/coverage.py
+++ rule_coverage/coverage.py
@@ -6,13 +6,15 @@ from git import Repo
 from git import Git
 from pathlib import Path
 
-repos=['sonar-abap','sonar-cpp','sonar-cobol','sonar-dotnet','sonar-css','sonar-flex','slang-enterprise','sonar-java','SonarJS','sonar-php','sonar-pli','sonar-plsql','sonar-python','sonar-rpg','sonar-swift','sonar-tsql','sonar-vb','sonar-html','sonar-xml','sonar-kotlin', 'sonar-secrets', 'sonar-security']
+REPOS=['sonar-abap','sonar-cpp','sonar-cobol','sonar-dotnet','sonar-css','sonar-flex','slang-enterprise','sonar-java','SonarJS','sonar-php','sonar-pli','sonar-plsql','sonar-python','sonar-rpg','sonar-swift','sonar-tsql','sonar-vb','sonar-html','sonar-xml','sonar-kotlin', 'sonar-secrets', 'sonar-security']
 
-canonical_names={
+CANONICAL_NAMES={
   'JS': 'JAVASCRIPT',
   'TS': 'TYPESCRIPT',
   'WEB': 'HTML'
 }
+
+RULES_FILENAME='covered_rules.json'
 
 def load_json(file):
   with open(file) as json_file:
@@ -33,8 +35,8 @@ def get_rules_json(path, languages, repoAndVersion):
         continue
 
 def canonicalize(language):
-  if language in canonical_names:
-    return canonical_names[language]
+  if language in CANONICAL_NAMES:
+    return CANONICAL_NAMES[language]
   return language
 
 def dump_rule(ruleId, rule, languages, repoAndVersion):
@@ -79,7 +81,7 @@ def dump_rules(repo, version):
     languages=sonarpedia['languages']
     simplify_spec_for_rules_that_are_still_supported()
     get_rules_json(path, languages, repo + ' ' + version)
-    with open(f"../{rules_filename}", 'w') as outfile:
+    with open(f"../{RULES_FILENAME}", 'w') as outfile:
       json.dump(rules, outfile, indent=2, sort_keys=True)
 
 def checkout_repo(repo):
@@ -125,16 +127,14 @@ def main():
   args = parser.parse_args()
 
   global rules
-  global rules_filename
-  rules_filename='covered_rules.json'
-  if os.path.exists(rules_filename):
-    rules=load_json(rules_filename)
+  if os.path.exists(RULES_FILENAME):
+    rules=load_json(RULES_FILENAME)
   else:
     rules={}
 
   if args.command[0] == "batchall":
-    print(f"batch mode for {repos}")
-    for repo in repos:
+    print(f"batch mode for {REPOS}")
+    for repo in REPOS:
       scan_all_versions(repo)
   elif args.command[0] == "batch":
     repo=args.command[1]
