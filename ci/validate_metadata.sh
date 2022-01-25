1bff1d460a3c165ce3bbe5529f972e7c650fc3b1
Marco Antognini <marco.antognini@sonarsource.com>
2022-01-25 16:45:31 +0100

    Cleanup validation CI script

--- ci/validate_metadata.sh
+++ ci/validate_metadata.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-set -x
 set -ueo pipefail
 
 invalid=0
 
-git fetch "${CIRRUS_DEFAULT_ORIGIN:-origin}" "${CIRRUS_DEFAULT_BRANCH:-master}"
+git fetch --quiet "${CIRRUS_DEFAULT_ORIGIN:-origin}" "${CIRRUS_DEFAULT_BRANCH:-master}"
 base="$(git merge-base FETCH_HEAD HEAD)"
+echo "Comparing against the merge-base: ${base}"
 modifiedRules="$(git diff --name-only "${base}" -- rules | sed -Ee 's#rules/(S[0-9]+)/.*#\1#' | sort -u)"
 
 cd rspec-tools
+pipenv install
 
-# Validate new and modified rules' metadata
+# Validate all metadata
+pipenv run rspec-tools validate-rules-metadata || invalid=1
+
+# Validate new or modified rules' metadata
 for rule in ${modifiedRules}
 do
   pipenv run rspec-tools validate-modified-rule-metadata "${rule}" || invalid=1
 done
-
-# Validate all metadata
-pipenv install
-pipenv run rspec-tools validate-rules-metadata || invalid=1
 
 exit "${invalid}"
