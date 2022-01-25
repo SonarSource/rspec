8e2333a53af66e66f3cf6c76e1d8cabfa5a7bc52
Marco Antognini <marco.antognini@sonarsource.com>
2022-01-25 14:15:47 +0100

    Update validate_metadata.sh to check modified rules specifically

--- ci/validate_metadata.sh
+++ ci/validate_metadata.sh
@@ -3,8 +3,19 @@ set -ueo pipefail
 
 invalid=0
 
-# Validate metadata
+git fetch --quiet "${CIRRUS_DEFAULT_ORIGIN:-origin}" "${CIRRUS_DEFAULT_BRANCH:-master}"
+base="$(git merge-base FETCH_HEAD HEAD)"
+modifiedRules="$(git diff --name-only "${base}" -- rules | sed -Ee 's#rules/(S[0-9]+)/.*#\1#' | sort -u)"
+
 cd rspec-tools
+
+# Validate new and modified rules' metadata
+for rule in ${modifiedRules}
+do
+  pipenv run rspec-tools validate-modified-rule-metadata "${rule}" || invalid=1
+done
+
+# Validate all metadata
 pipenv install
 pipenv run rspec-tools validate-rules-metadata || invalid=1
 
