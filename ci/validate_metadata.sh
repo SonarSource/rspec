99ee0cc4f809c7416dd9cd5786574852d8ed5bb4
Marco Antognini <marco.antognini@sonarsource.com>
2022-01-26 09:32:05 +0100

    Rename variables

--- ci/validate_metadata.sh
+++ ci/validate_metadata.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 set -ueo pipefail
 
-invalid=0
+exit_code=0
 
 git fetch --quiet "${CIRRUS_DEFAULT_ORIGIN:-origin}" "${CIRRUS_DEFAULT_BRANCH:-master}"
 base="$(git merge-base FETCH_HEAD HEAD)"
 echo "Comparing against the merge-base: ${base}"
-modifiedRules="$(git diff --name-only "${base}" -- rules | sed -Ee 's#rules/(S[0-9]+)/.*#\1#' | sort -u)"
+modified_rules="$(git diff --name-only "${base}" -- rules | sed -Ee 's#rules/(S[0-9]+)/.*#\1#' | sort -u)"
 
 cd rspec-tools
 pipenv install
 
 # Validate all metadata
-pipenv run rspec-tools validate-rules-metadata || invalid=1
+pipenv run rspec-tools validate-rules-metadata || exit_code=1
 
 # Validate new or modified rules' metadata
-for rule in ${modifiedRules}
+for rule in ${modified_rules}
 do
-  pipenv run rspec-tools validate-modified-rule-metadata "${rule}" || invalid=1
+  pipenv run rspec-tools validate-modified-rule-metadata "${rule}" || exit_code=1
 done
 
-exit "${invalid}"
+exit "${exit_code}"
