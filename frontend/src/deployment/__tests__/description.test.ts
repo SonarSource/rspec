c0c083853749fb0f70ac6cafba62055d7ef042d5
Marco Antognini <marco.antognini@sonarsource.com>
2022-02-02 17:54:45 +0100

    Prepare tests for description generation

--- frontend/src/deployment/__tests__/description.test.ts
+++ frontend/src/deployment/__tests__/description.test.ts
@@ -12,6 +12,16 @@ describe('description generation', () => {
         'S100/java/rule.adoc': [
           'include::../rule.adoc[]',
           'Specific content',
+          '',
+          '* See S101',
+          '* See RSPEC-101',
+        ].join('\n'),
+
+        'S101/rule.adoc': 'Generic content',
+        'S101/java/rule.adoc': [
+          'include::../rule.adoc[]',
+          'Specific content',
+          'See S100.'
         ].join('\n'),
 
         'S501/rule.adoc': 'Generic content, no active language',
@@ -23,31 +33,57 @@ describe('description generation', () => {
         const s100Java = path.join(dstPath, 'S100', 'java-description.html');
         expect(fs.existsSync(s100Java)).toBeTruthy();
         const htmlS100Java = fs.readFileSync(s100Java);
-        expect(htmlS100Java.toString()).toEqual([
-          '<div class="sect1">',
-          '<h2 id="_description">Description</h2>',
-          '<div class="sectionbody">',
-          '<div class="paragraph">',
-          '<p>Generic content',
-          'Specific content</p>',
-          '</div>',
-          '</div>',
-          '</div>',
-        ].join('\n'));
+        expect(htmlS100Java.toString()).toMatchInlineSnapshot(`
+"<div class=\\"sect1\\">
+<h2 id=\\"_description\\">Description</h2>
+<div class=\\"sectionbody\\">
+<div class=\\"paragraph\\">
+<p>Generic content
+Specific content</p>
+</div>
+<div class=\\"ulist\\">
+<ul>
+<li>
+<p>See S101</p>
+</li>
+<li>
+<p>See RSPEC-101</p>
+</li>
+</ul>
+</div>
+</div>
+</div>"
+`);
+
+        const s101Java = path.join(dstPath, 'S101', 'java-description.html');
+        expect(fs.existsSync(s101Java)).toBeTruthy();
+        const htmlS101Java = fs.readFileSync(s101Java);
+        expect(htmlS101Java.toString()).toMatchInlineSnapshot(`
+"<div class=\\"sect1\\">
+<h2 id=\\"_description\\">Description</h2>
+<div class=\\"sectionbody\\">
+<div class=\\"paragraph\\">
+<p>Generic content
+Specific content
+See S100.</p>
+</div>
+</div>
+</div>"
+`);
 
         const s501Default = path.join(dstPath, 'S501', 'default-description.html');
         expect(fs.existsSync(s501Default)).toBeTruthy();
         const htmlS501Default = fs.readFileSync(s501Default);
-        expect(htmlS501Default.toString()).toEqual([
-          '<div class="sect1">',
-          '<h2 id="_description">Description</h2>',
-          '<div class="sectionbody">',
-          '<div class="paragraph">',
-          '<p>Generic content, no active language</p>',
-          '</div>',
-          '</div>',
-          '</div>',
-        ].join('\n'));
+        expect(htmlS501Default.toString()).toMatchInlineSnapshot(`
+"<div class=\\"sect1\\">
+<h2 id=\\"_description\\">Description</h2>
+<div class=\\"sectionbody\\">
+<div class=\\"paragraph\\">
+<p>Generic content, no active language</p>
+</div>
+</div>
+</div>"
+`);
       });
     });
   });
