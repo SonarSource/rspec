2e7f232913003f4335907f5d8e718b8c0c477d43
Marco Antognini <89914223+marco-antognini-sonarsource@users.noreply.github.com>
2022-02-07 19:16:41 +0100

    RULEAPI-633: Autolink RSPECs

--- frontend/src/deployment/__tests__/description.test.ts
+++ frontend/src/deployment/__tests__/description.test.ts
@@ -9,10 +9,52 @@ describe('description generation', () => {
     return withTestDir((srcPath) => {
       createFiles(srcPath, {
         'S100/rule.adoc': 'Generic content',
-        'S100/java/rule.adoc': [
-          'include::../rule.adoc[]',
-          'Specific content',
-        ].join('\n'),
+        'S100/java/rule.adoc': `
+include::../rule.adoc[]
+Specific content
+
+== Test various forms of auto-link for RSPEC. S100
+
+* See S100, S101,S102.
+* See RSPEC-101
+* But not S103badref.
+* This is a code literal \`+S234+\` but this isn't S567.
+
+\\https://sonarsource.github.io/rspec/#/rspec/S100
+
+https://sonarsource.github.io/rspec/#/rspec/S100/cfamily
+
+https://sonarsource.github.io/rspec/#/rspec/S100/cfamily[]
+
+[source,cpp]
+----
+int foo() {
+  // No auto-links in code!
+  auto S100 = 100;
+  auto U100 = 100u;
+  return S100 + U100;
+}
+----
+
+After snippet, See S100.
+
+[source,cpp]
+----
+int goo() {
+  // No auto-links in code!
+  // S100
+}
+----
+
+more ref: RSPEC-200.
+`,
+
+        'S101/rule.adoc': 'Generic content',
+        'S101/java/rule.adoc': `
+include::../rule.adoc[]
+Specific content
+See S100.
+`,
 
         'S501/rule.adoc': 'Generic content, no active language',
       });
@@ -23,34 +65,109 @@ describe('description generation', () => {
         const s100Java = path.join(dstPath, 'S100', 'java-description.html');
         expect(fs.existsSync(s100Java)).toBeTruthy();
         const htmlS100Java = fs.readFileSync(s100Java);
-        expect(htmlS100Java.toString()).toEqual([
-          '<div class="sect1">',
-          '<h2 id="_description">Description</h2>',
-          '<div class="sectionbody">',
-          '<div class="paragraph">',
-          '<p>Generic content',
-          'Specific content</p>',
-          '</div>',
-          '</div>',
-          '</div>',
-        ].join('\n'));
+        expect(htmlS100Java.toString()).toMatchInlineSnapshot(`
+"<div class=\\"sect1\\">
+<h2 id=\\"_description\\">Description</h2>
+<div class=\\"sectionbody\\">
+<div class=\\"paragraph\\">
+<p>Generic content
+Specific content</p>
+</div>
+</div>
+</div>
+<div class=\\"sect1\\">
+<h2 id=\\"_test_various_forms_of_auto_link_for_rspec_s100\\">Test various forms of auto-link for RSPEC. <a data-rspec-id=\\"S100\\" class=\\"rspec-auto-link\\">S100</a></h2>
+<div class=\\"sectionbody\\">
+<div class=\\"ulist\\">
+<ul>
+<li>
+<p>See <a data-rspec-id=\\"S100\\" class=\\"rspec-auto-link\\">S100</a>, <a data-rspec-id=\\"S101\\" class=\\"rspec-auto-link\\">S101</a>,<a data-rspec-id=\\"S102\\" class=\\"rspec-auto-link\\">S102</a>.</p>
+</li>
+<li>
+<p>See <a data-rspec-id=\\"S101\\" class=\\"rspec-auto-link\\">RSPEC-101</a></p>
+</li>
+<li>
+<p>But not S103badref.</p>
+</li>
+<li>
+<p>This is a code literal <code>S234</code> but this isn&#8217;t <a data-rspec-id=\\"S567\\" class=\\"rspec-auto-link\\">S567</a>.</p>
+</li>
+</ul>
+</div>
+<div class=\\"paragraph\\">
+<p>https://sonarsource.github.io/rspec/#/rspec/S100</p>
+</div>
+<div class=\\"paragraph\\">
+<p><a href=\\"https://sonarsource.github.io/rspec/#/rspec/S100/cfamily\\" class=\\"bare\\">https://sonarsource.github.io/rspec/#/rspec/S100/cfamily</a></p>
+</div>
+<div class=\\"paragraph\\">
+<p><a href=\\"https://sonarsource.github.io/rspec/#/rspec/S100/cfamily\\" class=\\"bare\\">https://sonarsource.github.io/rspec/#/rspec/S100/cfamily</a></p>
+</div>
+<div class=\\"listingblock\\">
+<div class=\\"content\\">
+<pre class=\\"highlight\\"><code class=\\"language-cpp\\" data-lang=\\"cpp\\">int foo() {
+  // No auto-links in code!
+  auto S100 = 100;
+  auto U100 = 100u;
+  return S100 + U100;
+}</code></pre>
+</div>
+</div>
+<div class=\\"paragraph\\">
+<p>After snippet, See <a data-rspec-id=\\"S100\\" class=\\"rspec-auto-link\\">S100</a>.</p>
+</div>
+<div class=\\"listingblock\\">
+<div class=\\"content\\">
+<pre class=\\"highlight\\"><code class=\\"language-cpp\\" data-lang=\\"cpp\\">int goo() {
+  // No auto-links in code!
+  // S100
+}</code></pre>
+</div>
+</div>
+<div class=\\"paragraph\\">
+<p>more ref: <a data-rspec-id=\\"S200\\" class=\\"rspec-auto-link\\">RSPEC-200</a>.</p>
+</div>
+</div>
+</div>"
+`);
+
+        const s101Java = path.join(dstPath, 'S101', 'java-description.html');
+        expect(fs.existsSync(s101Java)).toBeTruthy();
+        const htmlS101Java = fs.readFileSync(s101Java);
+        expect(htmlS101Java.toString()).toMatchInlineSnapshot(`
+"<div class=\\"sect1\\">
+<h2 id=\\"_description\\">Description</h2>
+<div class=\\"sectionbody\\">
+<div class=\\"paragraph\\">
+<p>Generic content
+Specific content
+See <a data-rspec-id=\\"S100\\" class=\\"rspec-auto-link\\">S100</a>.</p>
+</div>
+</div>
+</div>"
+`);
 
         const s501Default = path.join(dstPath, 'S501', 'default-description.html');
         expect(fs.existsSync(s501Default)).toBeTruthy();
         const htmlS501Default = fs.readFileSync(s501Default);
-        expect(htmlS501Default.toString()).toEqual([
-          '<div class="sect1">',
-          '<h2 id="_description">Description</h2>',
-          '<div class="sectionbody">',
-          '<div class="paragraph">',
-          '<p>Generic content, no active language</p>',
-          '</div>',
-          '</div>',
-          '</div>',
-        ].join('\n'));
+        expect(htmlS501Default.toString()).toMatchInlineSnapshot(`
+"<div class=\\"sect1\\">
+<h2 id=\\"_description\\">Description</h2>
+<div class=\\"sectionbody\\">
+<div class=\\"paragraph\\">
+<p>Generic content, no active language</p>
+</div>
+</div>
+</div>"
+`);
       });
     });
   });
+
+  function normalizeString(str: string) {
+    // Ignore \n\r vs \n differences, and ignore extra whitespace.
+    return str.replace(/\r\n/g, '\n').trimEnd();
+  }
 
   expect.extend({
     toBeSameAsFile(received: string, expectedPath: string) {
@@ -61,7 +178,7 @@ describe('description generation', () => {
         };
       }
       const expected = fs.readFileSync(expectedPath).toString();
-      if (expected.replace(/\r\n/g, '\n') === received.replace(/\r\n/g, '\n')) {
+      if (normalizeString(expected) === normalizeString(received)) {
         return {
           // This message is used in case of test negation `expect(a).not.toBeSameAsFile(f)`
           message: () => `Identity check failed on ${expectedPath}.\nExpected:\n${expected}\n\nReceived:\n${received}`,
@@ -91,7 +208,7 @@ describe('description generation', () => {
           const expectedPath = path.join(__dirname, 'resources', 'metadata', ruleDir, file);
           expect(actual).toBeSameAsFile(expectedPath);
           treated++;
-        })
+        });
       });
       expect(treated).toBe(11);
     });
