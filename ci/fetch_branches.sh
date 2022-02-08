622d30f53b0317d6f6d23dfdfda32aeda3b18e9b
Marco Antognini <89914223+marco-antognini-sonarsource@users.noreply.github.com>
2022-02-08 12:15:47 +0100

    Update fetch_branches.sh

--- ci/fetch_branches.sh
+++ ci/fetch_branches.sh
@@ -2,12 +2,6 @@
 
 set -euo pipefail
 
-echo "SCRIPT DEBUG..."
-
-echo "BASE_BRANCH: $BASE_BRANCH"
-echo "DEFAULT_BRANCH: $DEFAULT_BRANCH"
-echo "REPO_CLONE_URL: $REPO_CLONE_URL"
-
 # When neither BASE_BRANCH nor DEFAULT_BRANCH are defined, fall back to "master".
 BRANCH="${BASE_BRANCH:-${DEFAULT_BRANCH:-master}}"
 git fetch "${REPO_CLONE_URL}" "+refs/heads/${BRANCH}:refs/remotes/origin/${BRANCH}"
