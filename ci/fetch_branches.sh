ec7e4f82c7f4540595ff42df88bf4f99e996408d
Marco Antognini <89914223+marco-antognini-sonarsource@users.noreply.github.com>
2022-02-08 12:24:38 +0100

    Update fetch_branches.sh

--- ci/fetch_branches.sh
+++ ci/fetch_branches.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 # When neither BASE_BRANCH nor DEFAULT_BRANCH are defined, fall back to "master".
 BRANCH="${BASE_BRANCH:-${DEFAULT_BRANCH:-master}}"
-git fetch "${REPO_CLONE_URL}" "+refs/heads/${BRANCH}:refs/remotes/origin/${BRANCH}"
+git fetch --force origin "${BRANCH}"
