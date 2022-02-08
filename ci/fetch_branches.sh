80245832df00861991f0ac209dee7f59a2e0d885
Marco Antognini <89914223+marco-antognini-sonarsource@users.noreply.github.com>
2022-02-08 12:13:27 +0100

    Update fetch_branches.sh

--- ci/fetch_branches.sh
+++ ci/fetch_branches.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 
 echo "SCRIPT DEBUG..."
 
-echo "CIRRUS_BASE_BRANCH: $CIRRUS_BASE_BRANCH"
-echo "CIRRUS_DEFAULT_BRANCH: $CIRRUS_DEFAULT_BRANCH"
-echo "CIRRUS_REPO_CLONE_URL: $CIRRUS_REPO_CLONE_URL"
+echo "BASE_BRANCH: $BASE_BRANCH"
+echo "DEFAULT_BRANCH: $DEFAULT_BRANCH"
+echo "REPO_CLONE_URL: $REPO_CLONE_URL"
 
-# When neither CIRRUS_BASE_BRANCH nor CIRRUS_DEFAULT_BRANCH are defined, fall back to "master".
-BRANCH="${CIRRUS_BASE_BRANCH:-${CIRRUS_DEFAULT_BRANCH:-master}}"
-git fetch "${CIRRUS_REPO_CLONE_URL}" "+refs/heads/${BRANCH}:refs/remotes/origin/${BRANCH}"
+# When neither BASE_BRANCH nor DEFAULT_BRANCH are defined, fall back to "master".
+BRANCH="${BASE_BRANCH:-${DEFAULT_BRANCH:-master}}"
+git fetch "${REPO_CLONE_URL}" "+refs/heads/${BRANCH}:refs/remotes/origin/${BRANCH}"
