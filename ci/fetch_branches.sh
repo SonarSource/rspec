a94f039ef546a41a4227ca282aed6c6a72bd5f16
Marco Antognini <89914223+marco-antognini-sonarsource@users.noreply.github.com>
2022-02-08 13:22:07 +0100

    investigate...

--- ci/fetch_branches.sh
+++ ci/fetch_branches.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 # When neither BASE_BRANCH nor DEFAULT_BRANCH are defined, fall back to "master".
 BRANCH="${BASE_BRANCH:-${DEFAULT_BRANCH:-master}}"
-git fetch --force origin "${BRANCH}"
+git fetch --force --all --prune
