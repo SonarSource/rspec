c66213024dee0e55c6218541666b87037487772e
Arseniy Zaostrovnykh <arseniy.zaostrovnykh@sonarsource.com>
2022-06-08 13:29:00 +0200

    Remove the extra verbosity

--- ci/validate_links.sh
+++ ci/validate_links.sh
@@ -12,22 +12,13 @@ ls -al $CACHE_PATH
 
 # validate the links in asciidoc
 cd rspec-tools
-echo "start testing"
-EXIT_CODE=2
-if pipenv install && pipenv run rspec-tools check-links --d ../out
-then
-    echo "The testing went fine"
+if pipenv install && pipenv run rspec-tools check-links --d ../out ; then
     EXIT_CODE=0
 else
-    echo "The testing resulted in an error"
     EXIT_CODE=1
 fi
-echo "Finish testing"
-echo "Hello!!!!"
 cd ..
-echo "Well, hello...."
 
 cp ./rspec-tools/link_probes.history "$CACHE_PATH/"
 
-echo "Exiting with $EXIT_CODE"
 exit $EXIT_CODE
