3878165238efbfcf6e3fc9fc62fab042125af949
Ilia Kebets <ilia.kebets@sonarsource.com>
2023-03-02 11:34:43 +0100

    bring changes from 'feature/education-new-checks' (except Lena's changes on S1481)

--- rspec-tools/tests/test_rules_repository.py
+++ rspec-tools/tests/test_rules_repository.py
@@ -7,7 +7,7 @@ from rspec_tools.errors import RuleNotFoundError
 def test_list_rules(mockrules: Path):
   '''Check that rules are all listed.'''
   rules = {rule.id for rule in RulesRepository(rules_path=mockrules).rules}
-  assert rules == {'S100', 'S101', 'S120', 'S4727', 'S1033'}
+  assert rules == {'S100', 'S101', 'S120', 'S200', 'S4727', 'S1033'}
 
 
 def test_list_languages(mockrules: Path):
@@ -33,5 +33,5 @@ def test_get_metadata(mockrules: Path):
 
 def test_nonexisting_rule(mockrules: Path):
   '''Check that a nonexisting rule is reported.'''
-  with pytest.raises(RuleNotFoundError, match=fr'^Cannot find rule S200'):
-    RulesRepository(rules_path=mockrules).get_rule('S200')
+  with pytest.raises(RuleNotFoundError, match=fr'^Cannot find rule S299'):
+    RulesRepository(rules_path=mockrules).get_rule('S299')
