3c4a3b0f4a8f9697514d6d82276c389911235a9d
Ilia Kebets <ilia.kebets@sonarsource.com>
2023-03-07 13:49:17 +0100

    Revert "don't fail on asciidoctor's warnings, but on fatal errors" and fix typo
    This reverts commit 5da5eb9f1e636def6555d09a652b9b32e74dacaf.

--- ci/validate_asciidoc.sh
+++ ci/validate_asciidoc.sh
@@ -97,8 +97,8 @@ done
 ADOC_COUNT=$(find rules -name "tmp*.adoc" | wc -l)
 
 if (( ADOC_COUNT > 0 )); then
-  if asciidoctor --failure-level=ERROR -o /dev/null rules/*/*/tmp*.adoc; then
-      if asciidoctor -a rspecator-view --failure-level=ERROR -o /dev/null rules/*/*/tmp*.adoc; then
+  if asciidoctor --failure-level=WARNING -o /dev/null rules/*/*/tmp*.adoc; then
+      if asciidoctor -a rspecator-view --failure-level=WARNING -o /dev/null rules/*/*/tmp*.adoc; then
           echo "${ADOC_COUNT} documents checked with success"
       else
           echo "ERROR: malformed asciidoc files in rspecator-view"
