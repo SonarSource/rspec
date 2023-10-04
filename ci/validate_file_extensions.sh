e8005de8846d1c107d90f6c0aaf42ed0a1290763
Cristian Ambrosini <cristian.ambrosini@sonarsource.com>
2023-10-04 09:23:33 +0200

    Fix unbound variable

--- ci/validate_file_extensions.sh
+++ ci/validate_file_extensions.sh
@@ -6,7 +6,7 @@ RULES_DIR="${TOPLEVEL}/rules"
 
 CSVB_FILES=$(find "${RULES_DIR}" -type f -name "*.cs" -o -name "*.vb")
 
-if [[ ${#CSVB_FILES[@]} -gt 0 ]]; then
+if [[ "${#CSVB_FILES[@]}" -gt 0 ]]; then
     echo "ERROR: '.cs' and/or '.vb' files are detected."
     echo $CSVB_FILES
     exit 1
