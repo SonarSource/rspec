f7d5735f5125b8b6a23a220b9780180b47a0d44c
Cristian Ambrosini <cristian.ambrosini@sonarsource.com>
2023-10-06 09:01:36 +0200

    Apply review comment

--- ci/validate_file_extensions.sh
+++ ci/validate_file_extensions.sh
@@ -7,7 +7,7 @@ CSVB_FILES=($(find "${RULES_DIR}" -type f -name "*.cs" -o -name "*.vb"))
 
 if [ ${#CSVB_FILES[@]} -gt 0 ]; then
     echo "ERROR: '.cs' and/or '.vb' files are detected."
-    echo $CSVB_FILES
+    printf '%s\n' "${CSVB_FILES[@]}"
     exit 1
 else 
     echo "SUCCESS: no '.cs' or '.vb' files detected."
