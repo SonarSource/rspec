acc56f1b03925451b021d09605110379b164813d
Antonio Aversa <antonio.aversa@sonarsource.com>
2024-02-21 17:31:05 +0100

    Fix Code Review 7

--- rules/S6930/S6930.AspNetCore.cs
+++ rules/S6930/S6930.AspNetCore.cs
@@ -134,6 +134,44 @@ public class WithAllTypesOfStrings : Controller
     public IActionResult WithInterpolatedRawStringLiteralsNotIncludingABackslash() => View();
 }
 
+public class WithHttpMethodAttributeAndAllTypesOfStrings : Controller
+{
+    private const string ASlash = "/";
+    private const string ABackslash = @"\";
+    private const string AConstStringIncludingABackslash = $"A{ABackslash}";
+    private const string AConstStringNotIncludingABackslash = $"A{ASlash}";
+
+    [HttpGet(AConstStringIncludingABackslash)]     // Noncompliant
+    public IActionResult WithConstStringIncludingABackslash() => View();
+
+    [HttpPost(AConstStringNotIncludingABackslash)] // Compliant
+    public IActionResult WithConstStringNotIncludingABackslash() => View();
+
+    [HttpPatch("\u002f[action]")]                  // Compliant: 2f is the Unicode code for '/'
+    public IActionResult WithEscapeCodeOfSlash() => View();
+
+    [HttpHead("\u005c[action]")]                   // Noncompliant: 5c is the Unicode code for '\'
+    public IActionResult WithEscapeCodeOfBackslash() => View();
+
+    [HttpDelete($"A{ASlash}[action]")]             // Compliant
+    public IActionResult WithInterpolatedString() => View();
+
+    [HttpOptions($@"A{ABackslash}[action]")]       // Noncompliant
+    public IActionResult WithInterpolatedVerbatimString() => View();
+
+    [HttpGet("""\[action]""")]                     // Noncompliant
+    public IActionResult WithRawStringLiteralsTriple() => View();
+
+    [HttpPost(""""\[action]"""")]                  // Noncompliant
+    public IActionResult WithRawStringLiteralsQuadruple() => View();
+
+    [HttpPatch($$"""{{ABackslash}}/[action]""")]   // Noncompliant
+    public IActionResult WithInterpolatedRawStringLiteralsIncludingABackslash() => View();
+
+    [HttpHead($$"""{{ASlash}}/[action]""")]        // Complaint
+    public IActionResult WithInterpolatedRawStringLiteralsNotIncludingABackslash() => View();
+}
+
 class WithMapControllerRoute
 {
     private const string ASlash = "/";
