e9c29abcf0b889ece3ed88effddff3dd51a39eab
Antonio Aversa <antonio.aversa@sonarsource.com>
2024-02-28 17:00:09 +0100

    Fix ASP.NET Framework MVC code examples

--- rules/S6931/S6931.AspNet4x.cs
+++ rules/S6931/S6931.AspNet4x.cs
@@ -1,6 +1,113 @@
 using System.Web.Mvc;
 
-// Parameterized test: one test per action, wrapped in its own controller
+[Route("[controller]")]
+public class NoncompliantController : Controller // Noncompliant {{Change the paths of the actions of this controller to be relative and adapt the controller route accordingly.}}
+//           ^^^^^^^^^^^^^^^^^^^^^^
+{
+    [Route("/Index1")]                  // Secondary
+    //     ^^^^^^^^^
+    public ActionResult Index1() => View();
+
+    [Route("/SubPath/Index2_1")]        // Secondary
+    //     ^^^^^^^^^^^^^^^^^^^
+    [Route("/[controller]/Index2_2")]   // Secondary
+    //     ^^^^^^^^^^^^^^^^^^^^^^^^        
+    public ActionResult Index2() => View();
+
+    [Route("/[action]")]                // Secondary
+    //     ^^^^^^^^^^^
+    public ActionResult Index3() => View();
+
+    [Route("/SubPath/Index4_1")]        // Secondary
+    //     ^^^^^^^^^^^^^^^^^^^
+    [Route("/[controller]/Index4_2")]   // Secondary
+    //     ^^^^^^^^^^^^^^^^^^^^^^^^
+    public ActionResult Index4() => View();
+}
+
+[Route("[controller]")]
+[Route("[controller]/[action]")]
+public class NoncompliantMultiRouteController : Controller // Noncompliant {{Change the paths of the actions of this controller to be relative and adapt the controller route accordingly.}}
+//           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+{
+    [Route("/Index1")]                  // Secondary
+    //     ^^^^^^^^^
+    public ActionResult Index1() => View();
+
+    [Route("/SubPath/Index2_1")]        // Secondary
+    //     ^^^^^^^^^^^^^^^^^^^
+    [Route("/[controller]/Index2_2")]   // Secondary
+    //     ^^^^^^^^^^^^^^^^^^^^^^^^    
+    public ActionResult Index2() => View();
+
+    [Route("/[action]")]                // Secondary
+    //     ^^^^^^^^^^^
+    public ActionResult Index3() => View();
+}
+
+[Route("[controller]")]
+public class CompliantController : Controller // Compliant: at least one action has at least a relative route
+{
+    [Route("/Index1")]
+    public ActionResult Index1() => View();
+
+    [Route("/SubPath/Index2")]
+    public ActionResult Index2() => View();
+
+    [Route("/[action]")]
+    public ActionResult Index3() => View();
+
+    [Route("/[controller]/Index4_1")]
+    [Route("SubPath/Index4_2")] // The relative route
+    public ActionResult Index4() => View();
+}
+
+public class NoncompliantNoControllerRouteController : Controller // Noncompliant {{Change the paths of the actions of this controller to be relative and add a controller route with the common prefix.}}
+{
+    [Route("/Index1")]                          // Secondary
+    public ActionResult Index1() => View();
+
+    [Route("/SubPath/Index2_1")]                // Secondary
+    [Route("/[controller]/Index2_2")]           // Secondary 
+    public ActionResult Index2() => View();
+
+    [Route("/[action]")]                        // Secondary
+    public ActionResult Index3() => View();
+}
+
+public class CompliantNoControllerRouteNoActionRouteController : Controller // Compliant
+{
+    public ActionResult Index1() => View(); // Default route -> relative
+
+    [Route("/SubPath/Index2")]
+    public ActionResult Index2() => View();
+
+    [Route("/[action]")]
+    public ActionResult Index3() => View();
+
+    [Route("/SubPath/Index4_1")]
+    [Route("/[controller]/Index4_2")]
+    public ActionResult Index4() => View();
+}
+
+public class CompliantNoControllerRouteEmptyActionRouteController : Controller // Compliant
+{
+    [Route]
+    public ActionResult Index1() => View(); // Empty route -> relative
+
+    [Route("/SubPath/Index2")]
+    public ActionResult Index2() => View();
+
+    [Route("/[action]")]
+    public ActionResult Index3() => View();
+
+    [Route("/SubPath/Index4_1")]
+    [Route("/[controller]/Index4_2")]
+    public ActionResult Index4() => View();
+}
+
+// Parameterized test: there should be one dedicated test per action, wrapped in its own controller.
+// The noncompliant/compliant comment should be moved from the action level to the controller level.
 [Route("[controller]")]
 public class BasicsController : Controller
 {
@@ -26,7 +133,8 @@ public class BasicsController : Controller
     public ActionResult WithMultipleRouteAttributes() => View();
 }
 
-// Parameterized test: one test per action, wrapped in its own controller
+// Parameterized test: there should be one dedicated test per action, wrapped in its own controller.
+// The noncompliant/compliant comment should be moved from the action level to the controller level.
 public class WithAttributeSyntaxVariationsController : Controller
 {
     [Route(template: @"/[action]", Name = "a", Order = 42)] // Noncompliant
@@ -49,18 +157,19 @@ namespace WithAliases
 
     public class WithAliasedRouteAttributeController : Controller // Noncompliant
     {
-        [MyRoute(@"/[controller]")]
+        [MyRoute(@"/[controller]")] // Secondary
         public ActionResult Index() => View();
     }
 
     public class WithFullQualifiedPartiallyAliasedNameController : Controller // Noncompliant
     {
-        [ASP.Mvc.RouteAttribute("A\\[action]")]
+        [ASP.Mvc.RouteAttribute("A\\[action]")] // Secondary
         public ActionResult Index() => View();
     }    
 }
 
-// Parameterized test: one test per action, wrapped in its own controller
+// Parameterized test: there should be one dedicated test per action, wrapped in its own controller.
+// The noncompliant/compliant comment should be moved from the action level to the controller level.
 public class WithAllTypesOfStringsController : Controller
 {
     private const string AConst = "A";
