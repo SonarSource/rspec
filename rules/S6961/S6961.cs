a9fb133767eb2859fa4cfa4d0dc1373c89189838
Gregory Paidis <gregory.paidis@sonarsource.com>
2024-04-08 11:18:01 +0200

    Review 1

--- rules/S6961/S6961.cs
+++ rules/S6961/S6961.cs
@@ -1,22 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 
-class MyApi : ApiControllerAttribute { }
-class MyApiDerived : ApiControllerAttribute { }
+class ChildAttribute : ApiControllerAttribute { }
+class GrandChildAttribute : ChildAttribute { }
 
-public class BaseController : Controller { }
-public class BaseControllerBase : ControllerBase { }
+public class ParentController : Controller { }
+public class ParentControllerBase : ControllerBase { }
 
-public class SecondBaseController : BaseController { }
-public class SecondBaseControllerBase : BaseControllerBase { }
+public class ChildController : ParentController { }
+public class ChildControllerBase : ParentControllerBase { }
 
 namespace SimpleCases
 {
     [ApiController]
-    public class Baseline : Controller { }               // Noncompliant {{Inherit from ControllerBase instead of Controller}}
+    public class Baseline : Controller { }              // Noncompliant {{Inherit from ControllerBase instead of Controller.}}
     //                      ^^^^^^^^^^
 
     [ApiController]
-    public class DerivesBase : ControllerBase { }       // Compliant
+    public class ChildOfBase : ControllerBase { }       // Compliant
 
     public class WithoutAttribute : Controller { }      // Compliant
 
@@ -34,107 +35,165 @@ namespace SimpleCases
 
     [ApiController]
     [NonController]
-    public class NotAController : Controller { }        // Compliant, see exception
+    public class NotAController : Controller { }        // Compliant, [NonController] is excluded
 
     [ApiController]
-    internal class Internal : Controller { }            // Compliant, see exception
+    internal class Internal : Controller { }            // Compliant, only raise at public methods
 }
 
 namespace SpecialAttributeUsages
 {
     [type: ApiController]
-    public class WithType : Controller { }          // Noncompliant {{Inherit from ControllerBase instead of Controller}}
+    public class WithType : Controller { }          // Noncompliant
     //                      ^^^^^^^^^^
     [ApiControllerAttribute]
-    public class WithSuffix : Controller { }        // Noncompliant {{Inherit from ControllerBase instead of Controller}}
+    public class WithSuffix : Controller { }        // Noncompliant
     //                        ^^^^^^^^^^
     [ApiController()]
-    public class WithParentheses : Controller { }   // Noncompliant {{Inherit from ControllerBase instead of Controller}}
+    public class WithParentheses : Controller { }   // Noncompliant
     //                             ^^^^^^^^^^
     [type: ApiControllerAttribute()]
-    public class Everything : Controller { }        // Noncompliant {{Inherit from ControllerBase instead of Controller}}
+    public class Everything : Controller { }        // Noncompliant
     //                        ^^^^^^^^^^
 }
 
 namespace Inheritance
 {
     [ApiController]
-    public class DerivesLevelOne : BaseController { }               // Compliant, we only check direct inheritance from "Controller"
+    public class Child : ParentController { }            // Compliant, we only check direct inheritance from "Controller"
 
     [ApiController]
-    public class DerivesLevelTwo : SecondBaseController { }         // Compliant
+    public class GrandChild : ChildController { }        // Compliant
 
     [ApiController]
-    public class NoInheritance { }                                  // Compliant
+    public class NoInheritance { }                       // Compliant
 }
 
 namespace CustomAttribute
 {
-    [MyApi]
-    public class UsesDerivedAttribute : Controller { }                  // Noncompliant {{Inherit from ControllerBase instead of Controller}}
-    //                                  ^^^^^^^^^^
+    [ChildAttribute]
+    public class UsesChildAttribute : Controller { }                 // Noncompliant
+    //                                ^^^^^^^^^^
 
-    [MyApiDerived]
-    public class UsesDerivedAttributeLevelTwo : Controller { }          // Noncompliant {{Inherit from ControllerBase instead of Controller}}
-    //                                          ^^^^^^^^^^
+    [GrandChildAttribute]
+    public class UsesGrandChildAttribute : Controller { }            // Noncompliant
+    //                                     ^^^^^^^^^^
 
-    [MyApi]
-    public class UsesDerivedAttributeBase : ControllerBase { }         // Compliant
+    [ChildAttribute]
+    public class UsesChildAttributeBase : ControllerBase { }         // Compliant
 
-    [MyApiDerived]
-    public class UsesDerivedAttributeBaseLevelTwo : ControllerBase { } // Compliant
+    [GrandChildAttribute]
+    public class UsesGrandChildAttributeBase : ControllerBase { }    // Compliant
 }
 
 namespace Partial
 {
-    public partial class Partial : Controller { }       // Noncompliant {{Inherit from ControllerBase instead of Controller}}
+    [ApiController]
+    public partial class Partial { }
+    public partial class Partial : Controller { }       // Noncompliant
     //                             ^^^^^^^^^^
 
     [ApiController]
-    public partial class Partial { }
-
-    public partial class PartialBase : Controller { }   // Compliant
+    public partial class PartialBase { }
+    public partial class PartialBase : ControllerBase { }   // Compliant
 
     [ApiController]
-    public partial class PartialBase { }                // Compliant
+    public partial class PartialDoubleInheritance : Controller { } // Noncompliant
+    //                                              ^^^^^^^^^^
+    public partial class PartialDoubleInheritance : Controller { } // Noncompliant as well
+    //                                              ^^^^^^^^^^
 }
 
 namespace Nested
 {
     public class Outer
     {
+        // TODO: If we manage to raise ONLY on non-nested classes, make this Compliant. (Delete this comment during implementation.)
         [ApiController]
-        public class Inner : Controller { }   // Compliant, this cannot be reached. It can be an FP, since I think it's very rare.
-        //                   ^^^^^^^^^^
+        public class Inner : Controller { }   // Noncompliant FP, this cannot be reached by ASP.NET.
     }
 }
 
-namespace Invocations
+namespace MemberUsages
 {
+    // TODO: Hey implementer, please extract this to a [DataTestMethod] test, with the right-part of the invocation as a parameter.
     [ApiController]
-    public class UsesView : Controller                   // Compliant
+    public class Invocations : Controller         // Compliant
     {
+        object model = null;
+
         public object Foo() => View();
-    }
+        public object Foo() => View("viewName");
+        public object Foo() => View(model);
+        public object Foo() => View("viewName", model);
 
-    [ApiController]
-    public class UsesPartialView : Controller            // Compliant
-    {
         public object Foo() => PartialView();
-    }
+        public object Foo() => PartialView("viewName");
+        public object Foo() => PartialView(model);
+        public object Foo() => PartialView("viewName", model);
 
-    [ApiController]
-    public class UsesViewComponent : Controller          // Compliant
-    {
         public object Foo() => ViewComponent("foo");
+        public object Foo() => ViewComponent("foo", model);
+        public object Foo() => ViewComponent(typeof(object));
+        public object Foo() => ViewComponent(typeof(object), model);
+
+        public object Foo() => Json(model);
+        public object Foo() => Json(model, model);
+
+        public object Foo() => OnActionExecutionAsync(default(ActionExecutingContext), default(ActionExecutionDelegate));
+    }
+
+    // TODO: Hey implementer, please extract this to a [DataTestMethod] test, with the right-part of the invocation as a parameter.
+    [ApiController]
+    public class VoidInvocations: Controller           // Compliant
+    {
+        public void Foo() => OnActionExecuted(default(ActionExecutedContext));
+        public void Foo() => OnActionExecuting(default(ActionExecutingContext));
+    }
+
+    // TODO: Hey implementer, please extract this to a [DataTestMethod] test, with the right-part of the invocation as a parameter.
+    [ApiController]
+    public class Properties: Controller                 // Compliant
+    {
+        public object Foo() => ViewData;
+        public object Foo() => ViewBag;
+        public object Foo() => TempData;
+
+        public object Foo() => ViewData["foo"];
+        public object Foo() => ViewBag["foo"];
+        public object Foo() => TempData["foo"];
     }
 
     [ApiController]
-    public class FakeViewInvocation : Controller        // Noncompliant {{Inherit from ControllerBase instead of Controller}}
-    //                                ^^^^^^^^^^
+    public class InConstructor: Controller              // Compliant
     {
-        public object Foo() => this.View();   // hides View, does not override it
-        public ViewResult View() => null;
+        object foo;
+        public InConstructor() => foo = View();
+    }
+
+    [ApiController]
+    public class InDestructor: Controller               // Compliant
+    {
+        object foo;
+        ~InDestructor() => foo = View();
+    }
+
+    [ApiController]
+    public class InPropertyGet: Controller              // Compliant
+    {
+        object foo => View();
+    }
+
+    [ApiController]
+    public class InPropertySet: Controller              // Compliant
+    {
+        object foo { set => _ = View(); }
+    }
+
+    [ApiController]
+    public class InIndexer: Controller                 // Compliant
+    {
+        object this[int index] => View();
     }
 
     [ApiController]
@@ -144,24 +203,58 @@ namespace Invocations
         public override ViewResult View() => null;
     }
 
-    public class NotInvoking : Controller               // Compliant
+    [ApiController]
+    public class FakeViewInvocation : Controller        // Noncompliant
+    //                                ^^^^^^^^^^
     {
-        public object NameOf() => nameof(View);
+        public object Foo() => this.View();   // hides View, does not override it
+        public ViewResult View() => null;
+    }
 
-        public void PassByName() => ExpectsAction(View); // Possible FP depending on what "ExpectsAction" does, just document it.
-
-        public void PassByLambda() => ExpectsAction(() => View()); // Same as above
-
-        public Func<ViewResult> NotCalled() => this.View; // Compliant, nothing is invoked
-
-        public void ExpectsAction(Func<ViewResult> func) { }
+    [ApiController]
+    public class BaseViewInvocation : Controller        // Compliant
+    {
+        public object Foo() => base.View();   // uses Controller.View
+        public ViewResult View() => null;
     }
 
     [ApiController]
     public partial class Partial { }
-    public partial class Partial : Controller { }     // Compliant
+    public partial class Partial : Controller { }       // Compliant
     public partial class Partial
     {
         public object Foo() => View();
+    }
+
+    [ApiController]
+    public class MemberReference : Controller           // Compliant
+    {
+        public Func<ViewResult> NotCalled() => this.View;   // nothing is invoked, but the dependency is used.
+    }
+
+    [ApiController]
+    public class NameOf : Controller                    // Compliant
+    {
+        public object Foo() => nameof(View); // same as above
+    }
+
+    [ApiController]
+    public class PassByName : Controller                // Compliant
+    {
+        public void Foo() => ExpectsAction(View);
+        public void ExpectsAction(Func<ViewResult> func)
+        {
+            // here func could be invoked or not.
+        }
+    }
+
+    [ApiController]
+    public class PassByLambda : Controller              // Compliant
+    {
+        public void Foo() => ExpectsAction(() => View());
+        public void ExpectsAction(Func<ViewResult> func)
+        {
+            // here func could be invoked or not.
+        }
     }
 }
