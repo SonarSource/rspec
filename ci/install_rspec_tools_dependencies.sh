f9b26ab3d97956cee2ccaf0c2acd58ece4b0789d
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2025-04-04 09:43:36 +0200

    Check python version

--- ci/install_rspec_tools_dependencies.sh
+++ ci/install_rspec_tools_dependencies.sh
@@ -2,13 +2,9 @@
 
 set -euo pipefail
 
-apt-get update
-apt-get install gcc libpq-dev -y
-apt-get install python3-dev python3-pip python3-venv python3-wheel -y
-pip install --upgrade pip
-pip3 install --upgrade pip
-pip3 install wheel
+python --version
 
 cd rspec-tools
+pipenv run python --version
 pipenv install --dev
 pipenv run pip install pytest pytest-cov
