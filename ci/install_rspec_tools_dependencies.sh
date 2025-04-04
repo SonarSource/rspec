1f84f3b67d88727b5a8fdbfa9970bb94ade9d535
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2025-04-04 08:16:39 +0200

    Another suggestion

--- ci/install_rspec_tools_dependencies.sh
+++ ci/install_rspec_tools_dependencies.sh
@@ -2,7 +2,11 @@
 
 set -euo pipefail
 
-pip install wheel
+sudo apt-get install gcc libpq-dev -y
+sudo apt-get install python-dev  python-pip -y
+sudo apt-get install python3-dev python3-pip python3-venv python3-wheel -y
+pip3 install wheel
+
 cd rspec-tools
 pipenv install --dev
 pipenv run pip install pytest pytest-cov
