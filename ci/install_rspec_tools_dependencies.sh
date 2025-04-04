72ebf2fbb77ab2a7199f27afa82d5af8f1236f9d
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2025-04-04 08:17:44 +0200

    Remove sudo

--- ci/install_rspec_tools_dependencies.sh
+++ ci/install_rspec_tools_dependencies.sh
@@ -2,9 +2,9 @@
 
 set -euo pipefail
 
-sudo apt-get install gcc libpq-dev -y
-sudo apt-get install python-dev  python-pip -y
-sudo apt-get install python3-dev python3-pip python3-venv python3-wheel -y
+apt-get install gcc libpq-dev -y
+apt-get install python-dev  python-pip -y
+apt-get install python3-dev python3-pip python3-venv python3-wheel -y
 pip3 install wheel
 
 cd rspec-tools
