2bf79b5fb53f626ef64d984c02777b15d7a6a76a
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2025-04-04 08:19:10 +0200

    Remove some packages

--- ci/install_rspec_tools_dependencies.sh
+++ ci/install_rspec_tools_dependencies.sh
@@ -2,8 +2,6 @@
 
 set -euo pipefail
 
-apt-get install gcc libpq-dev -y
-apt-get install python-dev  python-pip -y
 apt-get install python3-dev python3-pip python3-venv python3-wheel -y
 pip3 install wheel
 
