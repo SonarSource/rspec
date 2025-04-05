c052e1b31706fe97ac8508e607002dbe7758fcdb
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2025-04-05 07:58:48 +0200

    Reformat all files automatically
    I applied black and usort as follows:
    
    ```
    cd rspec-tools
    pipenv run black rspec_tools tests/
    pipenv run usort format rspec_tools tests
    ```

--- rspec-tools/rspec_tools/errors.py
+++ rspec-tools/rspec_tools/errors.py
@@ -2,20 +2,21 @@ from click import ClickException
 
 
 class InvalidArgumentError(ClickException):
-    '''Exception raised when an invalid argument is given to a CLI command.'''
+    """Exception raised when an invalid argument is given to a CLI command."""
 
     def __init__(self, message):
         super().__init__(message)
 
 
 class RuleValidationError(ClickException):
-    '''Exception raised when a rule did not pass validation.'''
+    """Exception raised when a rule did not pass validation."""
 
     def __init__(self, message):
         super().__init__(message)
 
+
 class RuleNotFoundError(ClickException):
-    '''Exception raised when a rule does not exist in the repository.'''
+    """Exception raised when a rule does not exist in the repository."""
 
     def __init__(self, message):
         super().__init__(message)
