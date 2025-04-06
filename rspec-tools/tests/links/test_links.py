34dfebfc1ce050b857da67122bfac51a61c756ea
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2025-04-06 11:00:38 +0200

    Enable test running from the repository root directory
    By making the paths used relative to the script files themselves.
    
    So now you don't have to change directory to rspec-tools, you can run
    directly from the rspec root directory:
    
    ```
    pytest rspec-tools
    ```

--- rspec-tools/tests/links/test_links.py
+++ rspec-tools/tests/links/test_links.py
@@ -1,11 +1,14 @@
 from click.testing import CliRunner
 from rspec_tools import cli
 from rspec_tools import checklinks
+from pathlib import Path
+
+FILE_DIR = Path(__file__).resolve().parent
 
 def test_find_urls():
   urls={}
-  checklinks.findurl_in_html("tests/links/404/S100/java/rule.html",urls)
-  assert urls == {'https://www.google.com/404': ['tests/links/404/S100/java/rule.html']}
+  checklinks.findurl_in_html(FILE_DIR / "404/S100/java/rule.html",urls)
+  assert urls == {'https://www.google.com/404': [FILE_DIR / '404/S100/java/rule.html']}
   assert len(urls) == 1
 
 def test_live():
@@ -16,28 +19,28 @@ def test_live():
 
 def test_404():
   runner = CliRunner()
-  result = runner.invoke(cli, ['check-links', '--d=tests/links/404'])
+  result = runner.invoke(cli, ['check-links', '--d', FILE_DIR / '404'])
   print(result.output)
   assert result.exit_code == 1
   assert "1/1 links are dead, see above ^^ the list and the related files" in result.output
 
 def test_url():
   runner = CliRunner()
-  result = runner.invoke(cli, ['check-links', '--d=tests/links/URL'])
+  result = runner.invoke(cli, ['check-links', '--d', FILE_DIR / 'URL'])
   print(result.output)
   assert result.exit_code == 1
   assert "1/1 links are dead, see above ^^ the list and the related files" in result.output
 
 def test_ok():
   runner = CliRunner()
-  result = runner.invoke(cli, ['check-links', '--d=tests/links/OK'])
+  result = runner.invoke(cli, ['check-links', '--d', FILE_DIR / 'OK'])
   print(result.output)
   assert result.exit_code == 0
   assert "All 1 links are good" in result.output
 
 def test_deprecated():
   runner = CliRunner()
-  result = runner.invoke(cli, ['check-links', '--d=tests/links/deprecated'])
+  result = runner.invoke(cli, ['check-links', '--d', FILE_DIR / 'deprecated'])
   print(result.output)
   assert result.exit_code == 0
   assert "All 1 links are good" in result.output
